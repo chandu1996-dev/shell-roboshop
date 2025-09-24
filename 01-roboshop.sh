85d832d70c98f22f416e585c2f6a865353831eeb
chandu kumar <chandukuttuboina@gmail.com>
2025-09-24 23:21:43 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -3,7 +3,7 @@
 Ami_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-06a7ef8cd626b5b3e"
 
-for instance in &@
+for instance in $@
 do
 INSTANCE_ID=$(aws ec2 run-instances \
     --image-id ami-09c813fb71547fc4f\
@@ -21,5 +21,7 @@ else
         IP=$(aws ec2 describe-instances --instance-ids i-0e800ac1272c3725c--query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
 fi
+echo ""instance :$IP"
+
 
 done
