d8ad31445d410bb8fcfbd6d3451d587b9c0c394d
chandu kumar <chandukuttuboina@gmail.com>
2025-09-25 08:43:13 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -2,7 +2,7 @@
 
 Ami_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-06a7ef8cd626b5b3e"
-
+ZONE_ID="Z00716421P5ZXLU5W7GIT"
 for instance in $@
 do
 
@@ -12,12 +12,31 @@ INSTANCE_ID=$(aws ec2 run-instances  --image-id $Ami_ID --instance-type t3.micro
 # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        #RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.born96.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        #RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
+        RECORD_NAME="$DOMAIN_NAME" # born96.fun
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 
 done
