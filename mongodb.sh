7e887c29e90234489be089a3755858f558b9328b
chandu kumar <chandukuttuboina@gmail.com>
2025-09-26 22:40:25 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -43,5 +43,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
     VALIDATE $? "Allow remote connections to mongodb"
 
-    systemctl restart monogodb $>>$LOG_FILE
+    systemctl restart monogod 
+    VALIDATE $? "Restart mongodb"
+
     
