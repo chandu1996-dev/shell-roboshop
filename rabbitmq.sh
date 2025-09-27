83d88cce79f9afd3d37cc8b5a7d1b85cba06fb0d
chandu kumar <chandukuttuboina@gmail.com>
2025-09-27 23:10:45 +0530

    rabbit

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-roboshop/mongodb.log
 start_TIME=$(date +%s)
-SCRIPT_DIR=$(PWD)
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
