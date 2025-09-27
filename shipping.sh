a29b75ef8f4f603951c66617fc307618b5214842
chandu kumar <chandukuttuboina@gmail.com>
2025-09-28 00:44:08 +0530

    rabbit

---
+++ shipping.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# set -euo pipefail
+
+# trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-roboshop/mongodb.log
+start_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install maven -y &>>$LOG_FILE
+
+id roboshop
+    if [ $? -ne 0 ]; then
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE   
+    VALIDATE $? "creating user"
+else
+ echo -e "User already exists ...$Y Skipping $N"
+fi
+
+mkdir -p /app  &>>$LOG_FILE    
+VALIDATE $? "creating a directory name as app"
+
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "downlod the cart application"
+
+cd /app 
+VALIDATE $? "change directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "unzip the code"
+
+mvn clean package &>>$LOG_FILE
+
+mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
+
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
+VALIDATE $? "copying systemctl services"
+
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable shipping &>>$LOG_FILE
+systemctl start shipping &>>$LOG_FILE
+
+dnf install mysql -y &>>$LOG_FILE
+
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+else
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart shipping
