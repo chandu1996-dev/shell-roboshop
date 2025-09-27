a120aba22dfc2cb169ad0a8edd39b44424469d48
chandu kumar <chandukuttuboina@gmail.com>
2025-09-27 20:53:01 +0530

    set and trap

---
+++ catalogue-set.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-roboshop/mongodb.log
+MONGODB_HOST="mongodb.born96.fun"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+# VALIDATE(){ # functions receive inputs through args just like shell script args
+#     if [ $1 -ne 0 ]; then
+#         echo -e  "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     else
+#         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+#     fi
+# }
+    
+    ###Nodejs######
+dnf module disable nodejs -y &>>$LOG_FILE
+# VALIDATE $? "disabling the nodejs"
+
+dnf  module enable nodejs:20 -y &>>$LOG_FILE
+# VALIDATE $? "enable the nodesjs"
+
+
+dnf installlll nodejs -y &>>$LOG_FILE
+# VALIDATE $? "install the modesjs"
+
+id roboshop
+    if [ $? -ne 0 ]; then
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOG_FILE   
+    VALIDATE $? "creating user"
+else
+ echo -e "User already exists ...$Y Skipping $N"
+fi
+
+mkdir -p /app  &>>$LOG_FILE    
+# VALIDATE $? "creating a directory name as app"
+
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+# VALIDATE $? "downlod the catalogue application"
+
+cd /app 
+# VALIDATE $? "change directory"
+
+rm -rf /app/*
+# VALIDATE $? "Removing existing code"
+
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+# VALIDATE $? "unzip the code"
+
+npm install &>>$LOG_FILE
+# VALIDATE $? "Install dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service 
+# VALIDATE $? "copying systemctl services"
+
+
+systemctl daemon-reload 
+
+systemctl enable catalogue &>>$LOG_FILE
+# VALIDATE $? "enable the catalogue"
+
+systemctl start catalogue &>>$LOG_FILE
+# VALIDATE $? "start the catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+# VALIDATE $? "copying the mongorepo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+# VALIDATE $? "install the mongodb"
+
+INDEX=$(mongosh mongodb.born96.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host mongodb.born96.fun </app/db/master-data.js &>>$LOG_FILE
+    # VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+# VALIDATE $? "Restarted catalogue"
+
